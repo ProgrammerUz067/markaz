e482898d4276156eb06ed61c9f10c98189accd0f
Diyor <baxacap@gmail.com>
2025-01-12 18:07:37 +0500

    map

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./components/About/About";
 import Teachers from "./components/Teachers/Teachers";
 import Courses from "./components/Courses/Courses";
 import Contact from "./components/Contact/Contact";
+import Map from "./components/Map/Map";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Teachers />
       <Courses />
       <Contact />
+      <Map />
     </div>
   );
 };
