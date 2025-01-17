eb7570294b47bcc5661ea8c720bdd829b098eb76
Diyor <baxacap@gmail.com>
2025-01-17 14:52:53 +0500

    2250px

--- src/components/Teachers/Teachers.jsx
+++ src/components/Teachers/Teachers.jsx
@@ -1,45 +1,34 @@
-import React from "react";
-import "./Teachers.css";
-import main2 from '../../img/main2.png'
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
-const Teachers = () => {
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import './Teachers.css'
+
+export default Teachers; () => {
   return (
-    <div className="teachers">
-      <h1 className="tittle">Наши учителя</h1>
-      <br />
-      <div className="container">
-        <div class="card">
-          <div class="content">
-            <h3>Рома Садыков</h3>
-            <p>Учитель по front-end разработки (rus)</p>
-          </div>
-        </div>
-
-        <div class="card2">
-          <div class="content">
-            <h3>Диёр Хасанов</h3>
-            <p>Учитель по front-end разработки (uzb)</p>
-          </div>
-        </div>
-
-        <div class="card3">
-          <div class="content">
-            <h3>Рамизжон Зиёдуллаев</h3>
-            <p>Учитель по back-end разработки (uzb)</p>
-          </div>
-        </div>
-
-        <div class="card4">
-          <div class="content">
-            <h3>Шахноза Назарова</h3>
-            <p>Учитель по Английский язык</p>
-          </div>
-        </div>
-      </div>
-
-      <img src={main2} alt="" className="main3" />
-    </div>
+    <Swiper
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      className='swiper'
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
   );
 };
-
-export default Teachers;
