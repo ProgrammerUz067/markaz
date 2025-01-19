4c27340839c90c49d6d95bb704b3dd7388cb031c
Diyor <baxacap@gmail.com>
2025-01-19 10:00:35 +0500

    2250px team

--- src/components/Teachers/Teachers.jsx
+++ src/components/Teachers/Teachers.jsx
@@ -1,34 +1,87 @@
-// import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
-import './Teachers.css'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-export default Teachers; () => {
+import "./Teachers.css";
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
+
+// import images
+import roma from "../../img/roma.png";
+import diyor from "../../img/diyor.png";
+import ramiz from "../../img/ramiz.png";
+
+const Teachers = () => {
   return (
-    <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-      className='swiper'
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
+    <>
+      <Swiper
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div class="card">
+            <div class="face face1">
+              <div class="content">
+                <img src={roma} />
+                <h3>Рома Садыков</h3>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <p>Учитель по front-end разработки</p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div class="card">
+            <div class="face face1">
+              <div class="content">
+                <img src={diyor} />
+                <h3>Диёр Хасанов</h3>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <p>Учитель по front-end разработки</p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div class="card">
+            <div class="face face1">
+              <div class="content">
+                <img src={ramiz} />
+                <h3>Рамизжон Зиёдуллаев</h3>
+              </div>
+            </div>
+            <div class="face face2">
+              <div class="content">
+                <p>Учитель по back-end разработки</p>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>shaxnoza</SwiperSlide>
+        <SwiperSlide>sevinch</SwiperSlide>
+        <SwiperSlide>matematika malim </SwiperSlide>
+        <SwiperSlide>sabina</SwiperSlide>
+        <SwiperSlide>timur</SwiperSlide>
+      </Swiper>
+    </>
   );
 };
+
+export default Teachers;
