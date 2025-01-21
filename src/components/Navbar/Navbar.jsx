6f72ea72e8ce93f1716ee0af8bdc1ff663518b9e
Diyor <baxacap@gmail.com>
2025-01-21 17:09:25 +0500

    iphone 14 pro max

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,29 @@
 import React from "react";
-import "./Navbar.css";
+import './Navbar.css'
 import logo from '../../img/logo.png'
 
 const Navbar = () => {
   return (
-    <div class="nav">
-      <input type="checkbox" id="nav-check" />
-      <div class="nav-header">
-        <div className="nav-title">
-          <img src={logo} alt="png" className="logo" />
+    <div>
+      <div class="nav">
+        <input type="checkbox" id="nav-check" />
+        <div class="nav-header">
+          <img src={logo} alt="" className="logo" />
         </div>
-      </div>
-      <div class="nav-btn">
-        <label for="nav-check">
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
-      </div>
+        <div class="nav-btn">
+          <label for="nav-check">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+        </div>
 
-      <div class="nav-links">
-        <a href="#">Главная</a>
-        <a href="#">О нас</a>
-        <a href="#">Курсы</a>
-        <a href="#">Контакты</a>
+        <div class="nav-links">
+          <a href="#">Главная</a>
+          <a href="#">О нас</a>
+          <a href="#">Курсы</a>
+          <a href="#">Контакты</a>
+        </div>
       </div>
     </div>
   );
